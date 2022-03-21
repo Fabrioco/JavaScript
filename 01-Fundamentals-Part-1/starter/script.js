8fb99e32db7570f96b5b248149d4f54b04655800
Fabrioco <Fabriciooliveiralopes50@gmail.com>
2022-03-20 21:32:53 -0300

    a

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,19 @@
+let js = 'amazing';
+      console.log(40+8+23-10);
+
+      console.log("Jonas");
+      console.log(23);
+
+    let firstName = ("Jonas");
+    console.log(firstName);
+
+    let years3 = 3;
+    console.log(years3);
+/*no js pode usar outras variaveis, para que possa ser chamada no console, alem de "firstName" */
+/* nessas variaveis n podem começar com um numero*/
+/* apenas _ e $ podem ser usados com simbolos e a palavra "new" e "function" n são permitidas;*/
+/* sempre ser o descrever as variaveis de forma mais especifica 
+CERTO:
+firstjob
+ERRADO:
+job1 */
