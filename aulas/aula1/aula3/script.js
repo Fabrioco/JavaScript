96b0ef67af2ef28b01905f885b05b730940b654c
Fabrioco <Fabriciooliveiralopes50@gmail.com>
2022-03-22 22:30:53 -0300

    a

---
+++ aulas/aula1/aula3/script.js
@@ -0,0 +1,10 @@
+let n1 = Number(prompt('digite um numero:'))
+// esse modelo tbm pode ser substituido por "let nome = prompt('Digite seu nome')"
+let n2 = Number(prompt('digite outro numero:'))
+
+let s = n1 + n2
+//alert('ola, ' + nome) para mostrar um pop-up com o nome
+alert(`A soma entre ${n1} e ${n2} é igual a  ${s}`)// para simplificar usa a Crase sempre no lugar das aspas
+
+// (number + number) para adição
+// (string + string) para concatenação
