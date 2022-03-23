e29cea7d55c0d0f8720f10b740b8be730bcb034b
Fabrioco <Fabriciooliveiralopes50@gmail.com>
2022-03-22 22:34:54 -0300

    a

--- aulas/Modulo 1/aula1/script.js
+++ aulas/Modulo 1/aula1/script.js
@@ -1,3 +1,3 @@
-alert('aqui tem porno apaga isso agora!')
-confirm('voce gosta de porno?')
-prompt('digite seu porno favorito')
+alert('Estou aprendendo JS')
+confirm('Voce gosta de JS?')
+prompt('Digite seu nome:')
