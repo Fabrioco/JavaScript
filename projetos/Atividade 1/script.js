6ae7b365422f69ea0938f4820c4d0c08dedbdc10
Fabrioco <Fabriciooliveiralopes50@gmail.com>
2022-03-30 21:43:02 -0300

    a

--- projetos/Atividade 1/script.js
+++ projetos/Atividade 1/script.js
@@ -1,8 +1,46 @@
-function clicar(){
-    var ano = document.getElementById('nas')
-    var nas = Number(ano.value)
-    console.log(nas)
-    idade = 2022 - nas 
-    console.log(idade)
-    res.inner
+function verificar(){
+   var data = new Date()
+   var ano = data.getFullYear()
+   var fano = document.getElementById('txtano')
+   var res = document.querySelector('div#res')
+   if (fano.value.length == 0 || Number(fano.value) > ano) {
+       alert('[ERRO] Verifique os dados e tente novamente')
+   } else{
+    var fsex = document.getElementsByName('radsex')
+    var idade = ano - Number(fano.value)
+    var genero = ''
+    
+    if (fsex[0].checked) {
+        genero = 'Homem'
+        if (idade >= 0 && idade < 10){
+            //criança
+            estado.innerHTML = 'grianza'
+        } else if (idade < 21){
+            //jovem
+            estado.innerHTML = 'grianza'
+        } else if (idade < 50){
+            //adulto
+            estado.innerHTML = 'crise da meia idade'
+        } else{
+            //idoso
+            estado.innerHTML = 'oq q há velin?'
+        }
+    }   else if (fsex[1].checked) {
+        genero = 'Mulher'
+        if (idade >= 0 && idade < 10){
+            //criança
+            estado.innerHTML = 'grianza'
+        } else if (idade < 21){
+            //jovem
+            estado.innerHTML = 'grianza'
+        } else if (idade < 50){
+            //adulto
+            estado.innerHTML = 'crise da meia idade'
+        } else{
+            //idoso
+            estado.innerHTML = 'oq q há velin?'
+        }
+    }
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+   }
 }
--- projetos/Atividade 2/script.js
+++ projetos/Atividade 2/script.js
@@ -1,9 +1,18 @@
-var agora = new Date()
-var agora = getHours()
-var agora = Number(hora.value)
-
-if (agora >= 5 && agora < 12){
-    res.innerText = `agora sao ${agora}`
-} else {
-    res.innerHTML = `vai dormir mlk`
+function carregar(){
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 22
+    msg.innerHTML = `agora sao ${hora} horas`
+        if(hora >= 5 && hora < 12){
+        img.src = 'amanhecer.jpg'
+        document.body.style.background = 'green'
+    }   else if (hora >=12 && hora < 18){
+        img.src = 'tarde.jpg'
+        document.body.style.background = 'red'
+    }   else {
+        img.src = 'noite.jpg'
+        document.body.style.background = 'blue'
+    }
 }
