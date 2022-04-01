70ac6824a39e5f68351fbbad3a0c6a325ff263a8
Fabrioco <Fabriciooliveiralopes50@gmail.com>
2022-04-01 19:20:11 -0300

    a

---
+++ aulas/modulo 5/aula1.js
@@ -0,0 +1,6 @@
+let num = [5, 6, 9, 7, 1]
+num.push(2)
+num.sort()
+console.log(`Nosso vetor é o ${num}`)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
